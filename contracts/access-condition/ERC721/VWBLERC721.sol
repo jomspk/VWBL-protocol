0081b460278031c2ccd06e694375969c1f4ffc71
Banri Yasui <69246274+jomspk@users.noreply.github.com>
2024-02-08 22:37:29 +0900

    change: コメントアウトしていたコードの削除

--- contracts/access-condition/ERC721/VWBLERC721.sol
+++ contracts/access-condition/ERC721/VWBLERC721.sol
@@ -28,26 +28,6 @@ contract VWBLERC721 is Ownable, AbstractVWBLToken, ERC721URIStorage {
         string memory _signMessage
     ) ERC721("VWBL", "VWBL") AbstractVWBLToken(_gatewayProxy, _accessCheckerContract, _signMessage) {}
 
-    // /**
-    //  * @notice Mint NFT, grant access feature and register access condition of digital content.
-    //  * @param _getKeyURl The URl of VWBL Network(Key management network)
-    //  * @param _documentId The Identifier of digital content and decryption key
-    //  */
-    // function mint(string memory _getKeyURl, bytes32 _documentId) public payable returns (uint256) {
-    //     uint256 tokenId = ++counter;
-    //     TokenInfo memory tokenInfo = TokenInfo(_documentId, msg.sender, _getKeyURl);
-    //     tokenIdToTokenInfo[tokenId] = tokenInfo;
-    //     _mint(msg.sender, tokenId);
-    //     // grant access control to nft and pay vwbl fee and register nft data to access control checker contract
-    //     IAccessControlCheckerByNFT(accessCheckerContract).grantAccessControlAndRegisterNFT{value: msg.value}(
-    //         _documentId,
-    //         address(this),
-    //         tokenId
-    //     );
-
-    //     return tokenId;
-    // }
-
     /**
      * @notice Mint NFT, grant access feature and register access condition of digital content.
      * @param _inputJson json data of diary NFT
