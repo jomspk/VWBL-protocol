7cb54821c86eace8544b563c333329ed231857dc
Banri Yasui <69246274+jomspk@users.noreply.github.com>
2024-02-15 15:45:37 +0900

    change: VWBLネットワークのキーとtokenIdの変更
    VWBLネットワークのキーを引数で取ることができていなかったので変更。
    AbstractVWBLToken.solでtokenIdが用意されていて、他のコードもそのトークンIdを使っているみたいだからバグを減らすため今の所はCounterを使わない方針にした

--- contracts/access-condition/ERC721/VWBLERC721.sol
+++ contracts/access-condition/ERC721/VWBLERC721.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 import {Base64} from "./libraries/Base64.sol";
 
@@ -16,10 +15,6 @@ import "../AbstractVWBLToken.sol";
  * @dev NFT which is added Viewable features that only NFT Owner can view digital content
  */
 contract VWBLERC721 is Ownable, AbstractVWBLToken, ERC721URIStorage {
-    using Counters for Counters.Counter;
-
-    Counters.Counter private _tokenIds;
-
     event NFTMinted(address sender, uint256 tokenId);
 
     constructor(
@@ -28,31 +23,38 @@ contract VWBLERC721 is Ownable, AbstractVWBLToken, ERC721URIStorage {
         string memory _signMessage
     ) ERC721("VWBL", "VWBL") AbstractVWBLToken(_gatewayProxy, _accessCheckerContract, _signMessage) {}
 
-    /**
+    /*
      * @notice Mint NFT, grant access feature and register access condition of digital content.
+     * @param _getKeyURl The URl of VWBL Network(Key management network)
      * @param _inputJson json data of diary NFT
      * @param _documentId The Identifier of digital content and decryption key
      */
-    function makeNFT(string memory _inputJson, bytes32 _documentId) public payable onlyOwner returns (uint256) {
-        uint256 newItemId = _tokenIds.current();
+    function makeNFT(
+        string memory _getKeyUrl,
+        string memory _inputJson,
+        bytes32 _documentId
+    ) public payable onlyOwner returns (uint256) {
+        uint256 tokenId = ++counter;
+
+        TokenInfo memory tokenInfo = TokenInfo(_documentId, msg.sender, _getKeyUrl);
+        tokenIdToTokenInfo[tokenId] = tokenInfo;
 
         //引数からそのままjsonデータを取得するようにする
-        string memory json = Base64.encode(bytes(string(abi.encodePacked(_inputJson))));
+        string memory json = Base64.encode(bytes(_inputJson));
 
         string memory finalTokenUri = string(abi.encodePacked("data:application/json;base64,", json));
 
-        _safeMint(msg.sender, newItemId);
-        _setTokenURI(newItemId, finalTokenUri);
-        _tokenIds.increment();
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, finalTokenUri);
 
-        emit NFTMinted(msg.sender, newItemId);
+        emit NFTMinted(msg.sender, tokenId);
         // grant access control to nft and pay vwbl fee and register nft data to access control checker contract
         IAccessControlCheckerByNFT(accessCheckerContract).grantAccessControlAndRegisterNFT{value: msg.value}(
             _documentId,
             address(this),
-            newItemId
+            tokenId
         );
 
-        return newItemId;
+        return tokenId;
     }
 }
